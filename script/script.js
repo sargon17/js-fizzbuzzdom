79df9fdc98d367a342fd2b776d54ce53cff83b4f
Mykhaylo (Michael) Tymofyeyev <mykhaylo.tymofyeyev@gmail.com>
2022-03-08 18:06:04 +0100

    commenting js

--- script/script.js
+++ script/script.js
@@ -6,12 +6,15 @@ for (let i = 1; i <= 100; i++) {
 
   if (i % 3 === 0 || i % 5 === 0) {
     if (i % 3 === 0 && i % 5 === 0) {
+      //what to do when buzz fizz
       tile.classList.add("mt__buzz-fizz");
       tile.innerHTML = `<p>buzz fizz</p>`;
     } else if (i % 3 === 0) {
+      //what to do when fizz
       tile.classList.add("mt__fizz");
       tile.innerHTML = `<p>fizz</p>`;
     } else {
+      //what to do when buzz buzz
       tile.classList.add("mt__buzz");
       tile.innerHTML = `<p>buzz</p>`;
     }
